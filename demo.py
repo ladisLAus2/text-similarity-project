88e74e965788c94076651a7c072e122b60c49858
Vlad Koliadiuk <koliadiuk_vlad@ukr.net>
2024-05-18 14:35:08 +0300

    syncher added

--- demo.py
+++ demo.py
@@ -1,3 +1,8 @@
 from text_similarity.logger import logging
 from text_similarity.exception import ExceptionHandler
 import sys
+from text_similarity.configuration.gcloud_syncer import GCloudSyncher
+
+syncher = GCloudSyncher()
+
+syncher.syncronize_cloud_to_folder("text-similarity-project", 'data.zip','download/data.zip')
