2460245a698486b004f82bf79ee948fa89a3fd1b
Vlad Koliadiuk <koliadiuk_vlad@ukr.net>
2024-05-18 12:37:34 +0300

    reqs added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='text-similarity-evaluation',
+    version='0.0.1',
+    author='Ladislaw',
+    author_email='vladvladvlad123555111@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
