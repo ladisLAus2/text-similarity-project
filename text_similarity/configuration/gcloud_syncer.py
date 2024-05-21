07338876fa0d20a8f9ba04d2fb7b30f0117ba158
Vlad Koliadiuk <koliadiuk_vlad@ukr.net>
2024-05-21 15:00:59 +0300

    updatet syncher with tqdm

--- text_similarity/configuration/gcloud_syncer.py
+++ text_similarity/configuration/gcloud_syncer.py
@@ -11,10 +11,16 @@ class GCloudSyncher:
     def download_file_from_cloud(self, blob_name, file_path, bucket_name):
         try:
             print(f'downloading file {blob_name} from cloud')
+            # bucket = self.client.get_bucket(bucket_name)
+            # blob = bucket.blob(blob_name)
+            # with open(file_path, 'wb') as f:
+            #     self.client.download_blob_to_file(blob, f)
             bucket = self.client.get_bucket(bucket_name)
             blob = bucket.blob(blob_name)
             with open(file_path, 'wb') as f:
-                self.client.download_blob_to_file(blob, f)
+                with tqdm.wrapattr(f, "write", total=blob.size) as file_obj:
+                    
+                    self.client.download_blob_to_file(blob, file_obj)
             return True
         except Exception as e:
             print(e)
@@ -25,7 +31,15 @@ class GCloudSyncher:
             print(f'uploading file {blob_name} to cloud')
             bucket = self.client.get_bucket(bucket_name)
             blob = bucket.blob(blob_name)
-            blob.upload_from_filename(file_path)
+            # blob.upload_from_filename(file_path)
+            with open(file_path, "rb") as in_file:
+                total_bytes = os.fstat(in_file.fileno()).st_size
+                with tqdm.wrapattr(in_file, "read", total=total_bytes, miniters=1, desc="upload to %s" % bucket_name) as file_obj:
+                    blob.upload_from_file(
+                        file_obj,
+                        size=total_bytes,
+                    )
+            
             return True
         except Exception as e:
             print(e)
